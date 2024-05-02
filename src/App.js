06eeb34fb4bc04dc2a9ca7ef66ad2ada1de6a45a
alineAmorim <apg.amorim@gmail.com>
2024-05-02 10:35:03 -0300

    Versão mini curso react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './app.css'
+
 
 function App() {
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  function calcularIMC(){
+    const alt = altura / 100;
+    const imc = (peso / (alt * alt)).toFixed(2);
+
+    if(imc < 18.5){
+      setMensagem("IMC: "+imc+ " (Abaixo do peso)");
+    }else if(imc >= 18.5 && imc < 25){
+      setMensagem("IMC: "+imc+ " (Peso normal)");
+    }else if(imc >= 25 && imc < 29.99){
+      setMensagem("IMC: "+imc+ " (Pré-Obesidade)");
+    }else if(imc >= 30 && imc < 34.99){
+      setMensagem("IMC: "+imc+ " (Obesidade Grau I)");
+    }else if(imc >= 35 && imc < 39.99){
+      setMensagem("IMC: "+imc+ " (Obesidade Grau II)");
+    }else if(imc >= 40){
+      setMensagem("IMC: "+imc+ " (Obesidade Grau III)");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Calculadora IMC</h1>
+      <span>Vamos calcular seu IMC</span>
+      <div className="area-input">
+        <input 
+          type="number" 
+          placeholder="Informe o peso em (kg) Ex: 90" 
+          value={peso}
+          onChange={ (e) => setPeso(e.target.value) }
+        />
+        <input 
+          type="number" 
+          placeholder="Informe a altura em (cm) Ex: 180" 
+          value={altura}
+          onChange={ (e) => setAltura(e.target.value) }
+          />
+        <button onClick={calcularIMC}>Calcular</button>
+      </div>
+      <h2>{mensagem}</h2>
     </div>
   );
 }
